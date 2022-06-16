2cffc7a5fc485e91061ad80956dfa6d4a42c1a55
harsh <harsh.sinha@pitt.edu>
2022-06-16 14:08:51 -0400

    understand loading

---
+++ tests/test_understand_loading.py
@@ -0,0 +1,3 @@
+from dicom2nifti import convert_directory
+dcm_folder_path = "C:\\Users\\harsh\\Datasets\\sinhah-20220610_152450\\147077\scans\\4-rsfMRI_RL\\resources\\DICOM"
+convert_directory(dcm_folder_path, dcm_folder_path)
